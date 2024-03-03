a544121a3f714029d911c7fc5254ff756c79e1bc
Surreal Gee <94802496+surrealgee@users.noreply.github.com>
2024-03-03 16:22:43 -0400

    Set working environment

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: "./dist",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "BMI Calculator",
+      template: ".src/index.html",
+    }),
+  ],
+};
